5a6d683d73f7ea18fa473a019d1612798ca61716
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2021-03-31 13:37:10 +0900

    Add plugin for logger

--- server1/src/server.ts
+++ server1/src/server.ts
@@ -2,9 +2,37 @@ import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
 import { context } from './context'
 
+const loggerPlugin = {
+
+  // Fires whenever a GraphQL request is received from a client.
+  requestDidStart(requestContext) {
+    console.log('Request started! Query:\n' +
+      requestContext.request.query);
+
+    return {
+
+      // Fires whenever Apollo Server will parse a GraphQL
+      // request to create its associated document AST.
+      parsingDidStart(requestContext) {
+        console.log('Parsing started!');
+      },
+
+      // Fires whenever Apollo Server will validate a
+      // request's document AST against your GraphQL schema.
+      validationDidStart(requestContext) {
+        console.log('Validation started!');
+      },
+
+    }
+  },
+};
+
 const server = new ApolloServer({
   schema: schema,
   context: context,
+  plugins: [
+    loggerPlugin
+  ]
 })
 
 server.listen().then(async ({ url }) => {
