59f50df3184bc43e05d84371eb393e6c0449e9c2
Katsuma Narisawa <katsuma.narisawa@gmail.com>
2021-03-31 13:22:46 +0900

    Remove unnecessary from gateway

--- gateway/src/server.ts
+++ gateway/src/server.ts
@@ -1,13 +1,11 @@
 import { ApolloServer } from 'apollo-server'
 import { schema } from './schema'
-import { context } from './context'
 
 const server = new ApolloServer({
   schema: schema,
-  context: context,
 })
 
-server.listen().then(async ({ url }) => {
+server.listen(3000).then(async ({ url }) => {
   console.log(`\
 🚀 Server ready at: ${url}
 ⭐️ See sample queries: http://pris.ly/e/ts/graphql#using-the-graphql-api
